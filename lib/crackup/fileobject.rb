05c26744c1ec3a1c923acc70ec555885b0bac6fa
Ryan Grove <ryan@wonko.com>
2006-11-15 23:42:07 +0000

    KISS

--- lib/crackup/fileobject.rb
+++ lib/crackup/fileobject.rb
@@ -1,118 +1,46 @@
 require 'crackup/fsobject'
 require 'digest/sha2'
 require 'fileutils'
-require 'time'
 
 module Crackup
 
   # Represents a file on the local filesystem.
   class FileObject
     include FileSystemObject
-    
-    attr_reader :path, :dir_path, :ctime, :mtime, :inode, :size, :timestamp,
-        :url
-  
-    #--
-    # Class Methods
-    #++
-    
-    def initialize(path, dir_path, hash, ctime, mtime, inode, size,
-        timestamp = Time.new.to_i)
-      super(path)
-      
-      @dir_path  = dir_path
-      @hash      = hash
-      @ctime     = ctime
-      @mtime     = mtime
-      @inode     = inode
-      @size      = size
-      @timestamp = timestamp
-      @url       = "#{Crackup.driver.url}/crackup_#{@path_hash}"
-    end
-    
-    # Gets a new Crackup::FileObject representing the Hash _row_, which should
-    # contain the contents of a database row.
-    def self.from_db(row)
-      return FileObject.new(row['path'], row['dir_path'], row['hash'],
-          row['ctime'], row['mtime'], row['inode'], row['size'], 
-          row['timestamp'])
-    end
-    
-    # Creates a new Crackup::FileObject representing the specified local
-    # filename.
-    def self.from_path(path)
-      unless File.file?(path)
-        raise ArgumentError, "#{path} is not a file"
-      end
-      
-      unless File.readable?(path)
-        raise Crackup::Error, "#{path} is not readable"
-      end
-      
-      stat = File.stat(path)
-      
-      return FileObject.new(path, File.dirname(path), nil, stat.ctime.to_i,
-          stat.mtime.to_i, stat.ino, stat.size)
-    end
-    
-    #--
-    # Instance Methods
-    #++
-    
-    def get_index_params(query_name)
-      case query_name
-        when :add
-          return {
-            ':path'      => @path,
-            ':dir_path'  => @dir_path,
-            ':name'      => @name,
-            ':hash'      => @hash,
-            ':ctime'     => @ctime,
-            ':mtime'     => @mtime,
-            ':inode'     => @inode,
-            ':size'      => @size,
-            ':timestamp' => Time.new.to_i
-          }
-        
-        when :delete
-          return {':path' => @path}
-        
-        when :delete_by_dir_path
-          return {':dir_path' => @dir_path}
-        
-      end
-    end
 
-    # Gets an SHA256 hash of the file's contents. The results of this method are
-    # cached, so subsequent calls will always return the same hash. 
-    def hash
-      return @hash unless @hash.nil?
+    attr_reader :file_hash, :url
+  
+    def initialize(filename)
+      unless File.file?(filename)
+        raise ArgumentError, "#{filename} is not a file"
+      end
       
-      digest = Digest::SHA256.new
+      super(filename)
       
-      File.open(@path, 'rb') do |file|
-        while buffer = file.read(1048576) do
-          digest << buffer
+      # Get the file's SHA256 hash.
+      digest = Digest::SHA256.new()
+      
+      File.open(filename, 'rb') do |file|
+        until file.eof? do
+          digest << file.read(1048576)
         end
       end
 
-      return @hash = digest.hexdigest()
-      
-    rescue => e
-      raise Crackup::Error, "Unable to generate file hash: #{e}"
+      @file_hash = digest.hexdigest()
+      @url       = "#{Crackup.driver.url}/crackup_#{@name_hash}"
     end
-
+    
     # Removes this file from the remote location.
     def remove
-      Crackup.debug "--> #{@path}"
+      Crackup.debug "--> #{@name}"
       Crackup.driver.delete(@url)
     end
 
     # Restores the remote copy of this file to the local path specified by
     # <em>path</em>.
     def restore(path)
-      path     = path.chomp('/') + '/' + File.dirname(@path).delete(':')
-      filename = path + '/' + @name
+      path     = path.chomp('/') + '/' + File.dirname(@name).delete(':')
+      filename = path + '/' + File.basename(@name)
 
       Crackup.debug "--> #{filename}"
       
@@ -139,15 +67,15 @@ module Crackup
     
     # Uploads this file to the remote location.
     def update
-      Crackup.debug "--> #{@path}"
+      Crackup.debug "--> #{@name}"
       
       # Compress/encrypt the file.
       tempfile = Crackup.get_tempfile()
       
       if Crackup.options[:passphrase].nil?
-        Crackup.compress_file(@path, tempfile)
+        Crackup.compress_file(@name, tempfile)
       else
-        Crackup.encrypt_file(@path, tempfile)
+        Crackup.encrypt_file(@name, tempfile)
       end
       
       # Upload the file.
